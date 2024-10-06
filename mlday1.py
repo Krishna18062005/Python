ade6f99c8955fe2342765aae3bc6403fe73a8b11
Krishna K <krish1826s@gmail.com>
2024-10-06 22:22:53 +0530

    Create mlday1.py

---
+++ mlday1.py
@@ -0,0 +1,9 @@
+#house price [prediciton ]
+size of the house --> price of the house 
+x1---> y1
+learned from labels is known as supervised learning
+rlation btwn input and output...
+--> Continuous 
+clustering ,no labels is knowm as unsuperwised learning 
+superwised --2 types --> regression,classification
+optimisation ---> Gradient desent algorithm
